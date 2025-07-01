e2743272684778191575e9b227fab66a8caf320c
Allister Sascha <allistersascha@gmail.com>
2025-07-01 14:41:50 -0500

    love the classics

--- day-2/homework/fizz-buzz.js
+++ day-2/homework/fizz-buzz.js
@@ -10,8 +10,27 @@
  *  c. AND for the multiples of five print “Buzz”.
  *  d. FINALLY, for numbers which are multiples of both three and five print “FizzBuzz”.
  */
+/*
+I: the numbers 1-100? none??
+O: same numbers but fizz for multiples of 3, buzz for mults of 5, and fizzbuzz for 15
+C: order of operations is key
+E:
+*/
 function fizzBuzz() {
   // YOUR CODE GOES BELOW HERE //
+  //set up a for loop starting at 1 and ending at 100
+  for (let i=1; i<=100; i++){
+    //do fizzbuzz first in conditional so it don't get twisted
+    if (i % 15 === 0){
+      console.log("FizzBuzz");
+    }else if (i % 3 === 0){
+      console.log("Fizz");
+    }else if (i % 5 === 0){
+      console.log("Buzz");
+    }else{
+      console.log(i);
+    }
+    }
   // YOUR CODE GOES ABOVE HERE //
 }
 
