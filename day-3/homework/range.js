63d6ad1416eeeb744477658a4b4756dd11a07a42
Allister Sascha <allistersascha@gmail.com>
2025-07-02 15:58:50 -0500

    it's a lil wet but it'll do

--- day-3/homework/range.js
+++ day-3/homework/range.js
@@ -21,8 +21,27 @@
  *  b. you'll need a loop, which one is best?
  *  c. you'll wanna make use of the push() method of Array.
  */
+/*
+I: two integers
+O: an array containing all the integers between them (inclusive)
+C: if 1st arg > 2nd arg, put them in reverse order
+E: 
+*/
 function range(start, end) {
   // YOUR CODE GOES BELOW HERE //
+  //make an empty array to put it in
+  let intergies = [];
+  if (start < end){
+    for (let i=start; i <= end; i++){
+      intergies.push(i);
+    }
+  }else if (end < start){
+    for (let i=start; i >= end; i--){
+      intergies.push(i);
+    }
+  }
+  return intergies;
+
   // YOUR CODE GOES ABOVE HERE //
 }
 
